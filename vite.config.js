dcc3a5f81c7578ea7a0b38f1bc56a061e43c2282
EldarMemetov <eldarm9347@gmail.com>
2024-10-04 10:22:37 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/green_energy/",
   build: {
     sourcemap: true,
   },
