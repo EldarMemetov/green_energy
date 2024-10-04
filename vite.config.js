493e46b60276733f175ad26cf3abc76d6b89f39e
EldarMemetov <eldarm9347@gmail.com>
2024-10-04 11:06:44 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-
-    base: command === "build" ? "/green_energy/" : "/",
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
