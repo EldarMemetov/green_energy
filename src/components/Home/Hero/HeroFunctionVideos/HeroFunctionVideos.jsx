39ae5740f5c2ada408a7bd3d8d3affdcba6bc220
EldarMemetov <eldarm9347@gmail.com>
2024-10-05 15:14:17 +0200

    update

--- src/components/Home/Hero/HeroFunctionVideos/HeroFunctionVideos.jsx
+++ src/components/Home/Hero/HeroFunctionVideos/HeroFunctionVideos.jsx
@@ -1,15 +1,48 @@
+import { useEffect, useRef } from "react";
 import styles from "./HeroFunctionVideos.module.css";
 import videoSrc from "../HeroVideos/heroVideo.mp4";
 
 const HeroFunctionVideos = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    // Проверка, является ли устройство мобильным
+    const isMobile = window.innerWidth <= 600;
+
+    // Программный запуск видео на мобильных устройствах
+    if (isMobile && videoRef.current) {
+      // Проверяем, можно ли воспроизвести видео
+      videoRef.current.play().catch((error) => {
+        console.error(
+          "Ошибка автозапуска видео на мобильном устройстве:",
+          error
+        );
+      });
+
+      // Добавляем обработчик touchstart для автозапуска при взаимодействии
+      const handleTouchStart = () => {
+        videoRef.current.play();
+      };
+
+      window.addEventListener("touchstart", handleTouchStart);
+
+      // Очищаем обработчик при размонтировании компонента
+      return () => {
+        window.removeEventListener("touchstart", handleTouchStart);
+      };
+    }
+  }, []);
+
   return (
     <section className={styles.sectionVideos}>
       <div className={styles.container}>
         <div className={styles.videoContainer}>
           <video
+            ref={videoRef}
             autoPlay
             loop
             muted
+            playsInline
             controls={false}
             className={styles.videoElement}
           >
